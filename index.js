dd38e213efb1d299ba9629ca377aee3cbd6f6b5e
Carlos Henrrique <carloshenrrique296@gmail.comm>
2024-04-30 19:37:23 +0100

    questions js

--- index.js
+++ index.js
@@ -7,4 +7,20 @@ $startGameButton.addEventListener("click",startGame)
 function startGame() {
     $startGameButton.classList.add("hider")
     $questionsContainer.classList.remove("hide")
+    displayNextQuestion()
 }
+
+
+
+
+const questions = [
+    {
+      question: "Quem é o maior artilheiro de todos os tempos da Copa do Mundo da FIFA?",
+      answers: [
+        { text: "<javascript>", correct: false },
+        { text: "<js>", correct: false },
+        { text: "<script>", correct: false },
+        { text: "<scripting>", correct: false }
+      ]  
+    },
+]
