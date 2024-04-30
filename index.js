77418c55f1a44135236a1feee52c788ebacf1dfd
Carlos Henrrique <carloshenrrique296@gmail.comm>
2024-04-30 19:44:38 +0100

    creat questions js

--- index.js
+++ index.js
@@ -12,15 +12,97 @@ function startGame() {
 
 
 
-
-const questions = [
-    {
-      question: "Quem é o maior artilheiro de todos os tempos da Copa do Mundo da FIFA?",
-      answers: [
-        { text: "<javascript>", correct: false },
-        { text: "<js>", correct: false },
-        { text: "<script>", correct: false },
-        { text: "<scripting>", correct: false }
-      ]  
-    },
-]
+    const questions = [
+        {
+            question: "Quem é o maior artilheiro de todos os tempos da Copa do Mundo da FIFA?",
+            answers: [
+                { text: "Miroslav Klose", correct: true },
+                { text: "Ronaldo Nazário", correct: false },
+                { text: "Pelé", correct: false },
+                { text: "Gerd Müller", correct: false }
+            ]
+        },
+        {
+            question: "Qual país ganhou a Copa do Mundo de 2018?",
+            answers: [
+                { text: "França", correct: true },
+                { text: "Brasil", correct: false },
+                { text: "Alemanha", correct: false },
+                { text: "Argentina", correct: false }
+            ]
+        },
+        {
+            question: "Qual jogador ganhou mais Bolas de Ouro?",
+            answers: [
+                { text: "Lionel Messi", correct: true },
+                { text: "Cristiano Ronaldo", correct: false },
+                { text: "Johan Cruyff", correct: false },
+                { text: "Michel Platini", correct: false }
+            ]
+        },
+        {
+            question: "Qual clube venceu mais Liga dos Campeões da UEFA?",
+            answers: [
+                { text: "Real Madrid", correct: true },
+                { text: "Milan", correct: false },
+                { text: "Liverpool", correct: false },
+                { text: "Barcelona", correct: false }
+            ]
+        },
+        {
+            question: "Qual jogador foi o mais jovem a participar de uma Copa do Mundo?",
+            answers: [
+                { text: "Pelé", correct: false },
+                { text: "Norman Whiteside", correct: true },
+                { text: "Samuel Eto'o", correct: false },
+                { text: "Michael Owen", correct: false }
+            ]
+        },
+        {
+            question: "Qual time venceu a primeira Copa do Mundo?",
+            answers: [
+                { text: "Uruguai", correct: true },
+                { text: "Brasil", correct: false },
+                { text: "Itália", correct: false },
+                { text: "Argentina", correct: false }
+            ]
+        },
+        {
+            question: "Que jogador é conhecido como 'El Pibe de Oro'?",
+            answers: [
+                { text: "Diego Maradona", correct: true },
+                { text: "Lionel Messi", correct: false },
+                { text: "Ronaldinho", correct: false },
+                { text: "Gabriel Batistuta", correct: false }
+            ]
+        },
+        {
+            question: "Qual jogador detém o recorde de mais gols em uma única edição de uma Copa do Mundo?",
+            answers: [
+                { text: "Just Fontaine", correct: true },
+                { text: "Ronaldo Nazário", correct: false },
+                { text: "Harry Kane", correct: false },
+                { text: "Miroslav Klose", correct: false }
+            ]
+        },
+        {
+            question: "Qual clube é conhecido como 'Os Invencíveis' na Premier League inglesa?",
+            answers: [
+                { text: "Chelsea", correct: false },
+                { text: "Manchester United", correct: false },
+                { text: "Arsenal", correct: true },
+                { text: "Liverpool", correct: false }
+            ]
+        },
+        {
+            question: "Quem foi o capitão da seleção espanhola que venceu a Copa do Mundo de 2010?",
+            answers: [
+                { text: "Xavi Hernandez", correct: false },
+                { text: "Iker Casillas", correct: true },
+                { text: "Sergio Ramos", correct: false },
+                { text: "Andres Iniesta", correct: false }
+            ]
+        }
+        
+    ];
+    
