76c0d38cd08e1f01d780fd87c1689ec95ee4b2ba
Carlos Henrrique <carloshenrrique296@gmail.comm>
2024-05-01 09:02:56 +0100

    java script add

--- index.js
+++ index.js
@@ -2,44 +2,61 @@ const $startGameButton = document.querySelector(".start-quiz")
 const $questionsContainer = document.querySelector(".questions-container")
 const $answersContainer = document.querySelector(".answers-container")
 const $questionText = document.querySelector(".question")
+const $nextQuestionButton = document.querySelector(".next-question")
 
 $startGameButton.addEventListener("click",startGame)
+$nextQuestionButton.addEventListener("click",displayNextQuestion)
 
 
 let currentQuestionIndex = 0
+let totalCorrect = 0
 
 
 function startGame() {
-    $startGameButton.classList.add("hider")
+    $startGameButton.classList.add("hide")
     $questionsContainer.classList.remove("hide")
     displayNextQuestion()
 }
 
 function displayNextQuestion() {
-   while(answersContainer.firstChild){
-    $answersContainer.removeChild($answersContainer.firstChild)
-   }
-}
+    resetState()
 
-questionText.textContet = questions[currentQuestionIndex].question
- questions[currentQuestionIndex].answers.forEach(answers=>{
+    if (questions.length === currentQuestionIndex) {
+        return finishGame()
+    }
+
+ $questionText.textContent = questions[currentQuestionIndex].question
+ questions[currentQuestionIndex].answers.forEach(answers=> {
   const newAnswer = document.createElement("button")
   newAnswer.classList.add("button", "answer")
-  newAnswer.textContent = answers.text
-  if (answers.correct){
-    newAnswer.dataset.correct = answers.correct
+  newAnswer.textContent = answer.text
+  if (answer.correct) {
+    newAnswer.dataset.correct = answer.correct
   }
   $answersContainer.appendChild(newAnswer)
 
   newAnswer.addEventListener("click", SelectionAnswer)
  })
 
+} 
 
- function SelectionAnswer(event) {
+ function resetState() {
+    while($answersContainer.firstChild){
+        $answersContainer.removeChild($answersContainer.firstChild)
+       }
+    
+    
+    document.body.removeAttribute("class")
+    $nextQuestionButton.classList.add("hide")
+ }
+
+
+function SelectionAnswer(event) {
     const answerClicked = event.target
 
     if (answerClicked.dataset.correct) {
         document.body.classList.add ("correct")
+        totalCorrect++
     } else {
         document.body.classList.add("incorrect")
     } 
@@ -50,8 +67,13 @@ questionText.textContet = questions[currentQuestionIndex].question
         } else {
             button.classList.add("incorrect")
         }
+
+        button.disabled = true
         
     })
+
+    $nextQuestionButton.classList.remove("hide")
+    currentQuestionIndex++
 }
  
 
@@ -150,3 +172,39 @@ questionText.textContet = questions[currentQuestionIndex].question
         
     ];
     
+    function finishGame() {
+        const totalQuestion = questions.length;
+        const performance = Math.floor(totalCorrect * 100 / totalQuestion);
+    
+        let message = "";
+    
+        switch (true) {
+            case (performance >= 90):
+                message = "Excellent :)";
+                break;
+            case (performance >= 70):
+                message = "Very Good :)";
+                break;
+            case (performance >= 50):
+                message = "Good";
+                break;
+            default:
+                message = "Study more :(";
+        }
+    
+        return message; // Adicionei uma declaração de retorno para que você possa usar a mensagem fora da função.
+    
+    
+
+       $questionsContainer.innerHTML =
+       `
+       <p>
+        You Right ${TotalCorrect} The ${totalQuestion} question!
+        <span> Results: ${message}</span>
+        <button>
+           Retake test 
+        </button>
+       </p>
+       `
+    
+    }        
